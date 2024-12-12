ce3e212263c76b566d31d8216a1dd56e46d541a0
Echo108471 <66149270+Echo108471@users.noreply.github.com>
2024-12-11 21:14:24 -0800

    Lvl2 Location Triggers

---
+++ monkeytrenchoffense/Scenes/second_level_trigger.gd
@@ -0,0 +1,18 @@
+class_name SecondLevelTrigger
+extends Area2D
+#@export var camera_location:Node2D
+@export var player_location:Node2D
+
+var player: Player
+var camera: Camera2D
+
+
+func _ready() -> void:
+	player = %Player
+	camera = $"../Player/Camera2D"
+
+func _on_body_entered(_body:Player) -> void:
+	# Position the player for the second level
+	player.position = player_location.position
+	# Remove this trigger node.
+	queue_free()
